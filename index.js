9a917f122d4467a6ea16613359ec69faf6afa29e
Thomas Jaggi <thomas.jaggi@unic.com>
2017-05-24 11:05:51 +0200

    Use gulp.watch instead of rebuilding it

--- index.js
+++ index.js
@@ -12,41 +12,37 @@ const defaults = {
   }
 }
 
-const fn = (options) => {
+// In order for gulp-cli to pick up the watcher, we need to pass the instance from gulpfile.js
+const fn = (options, gulp) => {
   const merge = require('lodash.merge')
-  const watch = require('glob-watcher')
-  const log = require('fancy-log')
-  const chalk = require('chalk')
-  const prettyTime = require('pretty-hrtime')
 
   const config = merge({}, defaults, options)
 
   if (!config.task.config.watch) {
-    return log(chalk.red('No watch path specified'))
+    return new Error('No watch path specified')
   }
 
-  const watcher = watch(config.task.config.watch, config.watcher)
+  // Create named callback function for gulp-cli to be able to log it
+  let cb = {
+    [config.task.name] () {
+      // Run task function with queued events as parameter
+      const task = config.task.fn(config.task.config, events)
+
+      // Reset events
+      events = []
+
+      return task
+    }
+  }
+
+  const watcher = gulp.watch(config.task.config.watch, config.watcher, cb[config.task.name])
+
+  let events = []
 
   watcher.on('all', function (event, path) {
-    const taskConfig = merge({}, config.task.config, {
-      watcher: {
-        event,
-        path
-      }
-    })
-    const start = process.hrtime()
-    let end
-    let time
-
-    // Log duration analogously to how gulp.start would do it
-    log('Starting', '\'' + chalk.cyan(config.task.name) + '\'...')
-
-    // Explicitly run task function and provide changed file as parameter
-    config.task.fn(taskConfig, function () {
-      end = process.hrtime(start)
-      time = prettyTime(end)
-
-      log('Finished', '\'' + chalk.cyan(config.task.name) + '\'', 'after', chalk.magenta(time))
+    events.push({
+      event,
+      path
     })
 
     // Close after first run if `once` is true
